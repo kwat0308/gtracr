abc04d5ec75df9464979b5bcfbbbdead9cffc16d
Keito <k.wat8973@gmail.com>
2020-06-10 13:30:43 -0600

    Moved B-field constants into constants.py

--- gtracr/constants.py
+++ gtracr/constants.py
@@ -7,3 +7,6 @@ import scipy.constants as sc
 SPEED_OF_LIGHT = sc.c
 
 ELEMENTARY_CHARGE = sc.e
+
+R_E = 6.371e6  # earth radius (meters)
+g10 = -29404.8 * (1e-9) # B-field parameter from IGRF 2020 (in Teslas)
