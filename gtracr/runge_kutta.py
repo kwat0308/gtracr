4fc0403086fa82220d4063c99ff9dcd3927aa3cb
Keito <k.wat8973@gmail.com>
2020-06-23 09:12:39 +0900

    Changed back DE

--- gtracr/runge_kutta.py
+++ gtracr/runge_kutta.py
@@ -33,70 +33,74 @@ from gtracr.constants import SPEED_OF_LIGHT
 # obtained from D.F.Smart, M.A.Shea, Sept. 1, 2004
 # radial velocity DE
 def dvrdt(t, r, theta, phi, vr, vtheta, vphi, particle):
-    # term1 = particle.charge * (
-    #     vtheta * B_phi(r, theta) - B_theta(r, theta) * vphi) / (
-    #         particle.mass * gamma(vmag_spherical(vr, vtheta, vphi, r, theta)))
-    # term2 = vtheta**2. / r
-    # term3 = vphi**2. / r
-    gam = gamma(vmag_spherical(vr, vtheta, vphi, r, theta))
-    lor_consts = particle.charge / (particle.mass*gam*SPEED_OF_LIGHT**2.)
-    # term1 = particle.charge * (
-    #     vtheta * B_phi(r, theta, phi) - B_theta(r, theta, phi) * vphi) / (
-    #         particle.mass * gam)
+    term1 = particle.charge * (
+        vtheta * B_phi(r, theta, phi) - B_theta(r, theta, phi) * vphi) / (
+            particle.mass * gamma(vmag_spherical(vr, vtheta, vphi, r, theta)))
+    term2 = vtheta**2. / r
+    term3 = vphi**2. / r
 
-    lor_term1 = (vtheta * B_phi(r, theta, phi) - B_theta(r, theta, phi) * vphi) * (SPEED_OF_LIGHT**2. - vr**2.)
-    lor_term2 = r*vr*vtheta*(B_phi(r, theta, phi) * vr - vphi * B_r(r, theta, phi))
-    lor_term3 = r*vr*vphi*np.sin(theta)*(B_theta(r, theta, phi) * vr - B_r(r, theta, phi) * vtheta)
+    return term1 + term2 + term3
 
-    accel_term1 = r*vtheta**2.
-    accel_term2 = r*np.sin(theta)**2.*vphi**2.
 
-    return accel_term1 + accel_term2 + lor_consts*(lor_term1 - lor_term2 + lor_term3)
+    # gam = gamma(vmag_spherical(vr, vtheta, vphi, r, theta))
+    # lor_consts = particle.charge / (particle.mass*gam*SPEED_OF_LIGHT**2.)
+    # # term1 = particle.charge * (
+    # #     vtheta * B_phi(r, theta, phi) - B_theta(r, theta, phi) * vphi) / (
+    # #         particle.mass * gam)
+
+    # lor_term1 = (vtheta * B_phi(r, theta, phi) - B_theta(r, theta, phi) * vphi) * (SPEED_OF_LIGHT**2. - vr**2.)
+    # lor_term2 = r*vr*vtheta*(B_phi(r, theta, phi) * vr - vphi * B_r(r, theta, phi))
+    # lor_term3 = r*vr*vphi*np.sin(theta)*(B_theta(r, theta, phi) * vr - B_r(r, theta, phi) * vtheta)
+
+    # accel_term1 = r*vtheta**2.
+    # accel_term2 = r*np.sin(theta)**2.*vphi**2.
+
+    # return accel_term1 + accel_term2 + lor_consts*(lor_term1 - lor_term2 + lor_term3)
             
-    # return term1 + term2 + term3
+    
 
 
 # theta component velocity DE
 def dvthetadt(t, r, theta, phi, vr, vtheta, vphi, particle):
-    # term1 = particle.charge * (vphi * B_r(r, theta, phi) - B_phi(r, theta, phi) * vr) / (
-    #     particle.mass * gamma(vmag_spherical(vr, vtheta, vphi, r, theta)))
-    # term2 = (vr * vtheta) / r
-    # term3 = vphi**2. / (r * np.tan(theta))
-    # return term1 - term2 + term3
+    term1 = particle.charge * (vphi * B_r(r, theta, phi) - B_phi(r, theta, phi) * vr) / (
+        particle.mass * gamma(vmag_spherical(vr, vtheta, vphi, r, theta)))
+    term2 = (vr * vtheta) / r
+    term3 = vphi**2. / (r * np.tan(theta))
+    return term1 - term2 + term3
 
-    gam = gamma(vmag_spherical(vr, vtheta, vphi, r, theta))
-    lor_consts = particle.charge / (particle.mass*gam*SPEED_OF_LIGHT**2.)
+    # gam = gamma(vmag_spherical(vr, vtheta, vphi, r, theta))
+    # lor_consts = particle.charge / (particle.mass*gam*SPEED_OF_LIGHT**2.)
 
-    lor_term1 = r*vr*vtheta*(vtheta * B_phi(r, theta, phi) - B_theta(r, theta, phi) * vphi)
-    lor_term2 = (B_phi(r, theta, phi) * vr - vphi * B_r(r, theta, phi))*(SPEED_OF_LIGHT**2. - (r*vtheta)**2.)
-    lor_term3 = r**2.*vtheta*vphi*np.sin(theta)*(B_theta(r, theta, phi) * vr - B_r(r, theta, phi) * vtheta)
+    # lor_term1 = r*vr*vtheta*(vtheta * B_phi(r, theta, phi) - B_theta(r, theta, phi) * vphi)
+    # lor_term2 = (B_phi(r, theta, phi) * vr - vphi * B_r(r, theta, phi))*(SPEED_OF_LIGHT**2. - (r*vtheta)**2.)
+    # lor_term3 = r**2.*vtheta*vphi*np.sin(theta)*(B_theta(r, theta, phi) * vr - B_r(r, theta, phi) * vtheta)
 
-    accel_term1 = 2.*vr*vtheta
-    accel_term2 = r*vphi**2.*np.sin(theta)*np.cos(theta)
+    # accel_term1 = 2.*vr*vtheta
+    # accel_term2 = r*vphi**2.*np.sin(theta)*np.cos(theta)
 
-    return (accel_term2 - accel_term1 + lor_consts*(lor_term1 - lor_term2 + lor_term3)) / r
+    # return (accel_term2 - accel_term1 + lor_consts*(lor_term1 - lor_term2 + lor_term3)) / r
 
 
 # phi comp vel/. DE
 def dvphidt(t, r, theta, phi, vr, vtheta, vphi, particle):
-    # term1 = particle.charge * (
-    #     B_theta(r, theta, phi) * vr - B_r(r, theta, phi) * vtheta) / (
-    #         particle.mass * gamma(vmag_spherical(vr, vtheta, vphi, r, theta)))
-    # term2 = (vr * vphi) / r
-    # term3 = (vtheta * vphi) / (r * np.tan(theta))
-    # return term1 - term2 - term3
+    term1 = particle.charge * (
+        B_theta(r, theta, phi) * vr - B_r(r, theta, phi) * vtheta) / (
+            particle.mass * gamma(vmag_spherical(vr, vtheta, vphi, r, theta)))
+    term2 = (vr * vphi) / r
+    term3 = (vtheta * vphi) / (r * np.tan(theta))
+    return term1 - term2 - term3
 
-    gam = gamma(vmag_spherical(vr, vtheta, vphi, r, theta))
-    lor_consts = particle.charge / (particle.mass*gam*SPEED_OF_LIGHT**2.)
+    # gam = gamma(vmag_spherical(vr, vtheta, vphi, r, theta))
+    # lor_consts = particle.charge / (particle.mass*gam*SPEED_OF_LIGHT**2.)
 
-    lor_term1 = r*vr*vphi*np.sin(theta)*(vtheta * B_phi(r, theta, phi) - B_theta(r, theta, phi) * vphi)
-    lor_term2 = r**2.*vphi*vtheta*np.sin(theta)*(B_phi(r, theta, phi) * vr - vphi * B_r(r, theta, phi))
-    lor_term3 = (B_theta(r, theta, phi) * vr - B_r(r, theta, phi) * vtheta)*(SPEED_OF_LIGHT**2. - (r*vphi*np.sin(theta))**2.)
+    # lor_term1 = r*vr*vphi*np.sin(theta)*(vtheta * B_phi(r, theta, phi) - B_theta(r, theta, phi) * vphi)
+    # lor_term2 = r**2.*vphi*vtheta*np.sin(theta)*(B_phi(r, theta, phi) * vr - vphi * B_r(r, theta, phi))
+    # lor_term3 = (B_theta(r, theta, phi) * vr - B_r(r, theta, phi) * vtheta)*(SPEED_OF_LIGHT**2. - (r*vphi*np.sin(theta))**2.)
 
-    accel_term1 = 2.*vr*vphi*np.sin(theta)
-    accel_term2 = 2.*r*vtheta*vphi*np.cos(theta)
+    # accel_term1 = 2.*vr*vphi*np.sin(theta)
+    # accel_term2 = 2.*r*vtheta*vphi*np.cos(theta)
 
-    return (- accel_term2 - accel_term1 + lor_consts*(lor_term1 - lor_term2 + lor_term3)) / (r*np.sin(theta))
+    # return (- accel_term2 - accel_term1 + lor_consts*(lor_term1 - lor_term2 + lor_term3)) / (r*np.sin(theta))
 
 
 
